cccda2808d8bb8538976e25e480c2d5aa82b8d33
Rúben França <ruben_tiagof@hotmail.com>
2019-05-26 18:42:16 +0100

    Add header component

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -4,7 +4,7 @@ import Header from './Header';
 
 export default props =>
     <Fragment>
-        <Header />
+        <Header {...props} />
         <main className="content">
             Content
         </main>
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import React from 'react';
 import Logo from '../components/templates/Logo';
@@ -9,6 +11,6 @@ export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="Start" subtitle="Insert subtitle here" />
         <Footer />
     </div>
