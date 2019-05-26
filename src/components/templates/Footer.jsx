1bf8153e7f12656ccb5e46d4495451df357bacd0
Rúben França <ruben_tiagof@hotmail.com>
2019-05-26 18:47:57 +0100

    Add footer

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react';
 
 export default props => 
     <footer className='footer'>
-        Footer
+        <span>
+            Developed with <i className="fa fa-heart text-danger"></i> by <strong>Rúben França</strong>
+        </span>
     </footer>
